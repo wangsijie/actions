bb753c5bb5b5e8f3e7ca73ae2d1bab838a9e3103
wangsijie <i@sijie.wang>
2021-08-16 13:30:58 +0800

    fix total

--- packages/eth/binance.js
+++ packages/eth/binance.js
@@ -6,6 +6,6 @@ module.exports.getPoolInfo = async () => {
   const res2 = await axios.get(`https://pool.binance.com/mining-api/v1/public/pool/miner/index?observerToken=${process.env.BN_TOKEN}`);
   const { workerDatas } = res2.data.data;
   const yyyRate = Math.floor(workerDatas.find(worker => worker.workerName === '0003').dayHashRate / 1000000 * 0.41);
-  const totalRate = Math.floor(workerDatas.reduce((p, worker) => worker.dayHashRate, 0) / 1000000);
+  const totalRate = Math.floor(workerDatas.reduce((p, worker) => p + worker.dayHashRate, 0) / 1000000);
   return { invalidNum, paid: Number(profitYesterday.ETH), yyyRate, totalRate };
 }
